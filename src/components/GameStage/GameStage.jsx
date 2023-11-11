9040dc026556418752a10934a4ae5c9977551e5d
nuno-oliveira <nuno.oliveira.1991@gmail.com>
2023-11-11 00:04:55 +0000

    GameStage component update, temporary syles added

--- src/components/GameStage/GameStage.jsx
+++ src/components/GameStage/GameStage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import { useGameStatsContext } from '../../contexts/GameStatsContext';
 import { v4 as uuidv4 } from 'uuid';
 import style from './game-stage-styles.module.scss';
+import { diceBox } from './../../constants.js'
 
 
 const GameStage = () => {
@@ -20,6 +21,8 @@ const GameStage = () => {
     setPlayButtonMessage,
     rollResult,
     setRollResult,
+    rollCounter,
+    setRollCounter,
     brains,
     setBrains,
     shots,
@@ -29,66 +32,90 @@ const GameStage = () => {
   const playButtonRef = useRef(null);
   const skipButtonRef = useRef(null);
 
-  let handSize = 3;
-
   const drawDice = () => {
-    if (diceInsideBox.length < 3) handSize = diceInsideBox.length;
-    else if (diceInsideBox.length === 0) {
-      console.log('Dice box is empty, you lost the game!');
-      return;
+    let handSize = 3;
+    if (diceInsideBox.length < 3) { 
+      console.log('Dice box is empty, you must end your turn!');
+      endTurn();
     };
+    const updatedDiceInsideBox = [...diceInsideBox];
+    const drawnDice = [];
     for (let i = 0; i < handSize; i++) {
-      let randomDieIndex = Math.floor(Math.random() * diceInsideBox.length);
-      diceInsideBox.splice(randomDieIndex, 1);
-      let die = diceInsideBox[randomDieIndex]
-      playerHand.push(die);
-      setDiceInsideBox(diceInsideBox)
+      let randomDieIndex = Math.floor(Math.random() * updatedDiceInsideBox.length);
+      let die = updatedDiceInsideBox[randomDieIndex];
+      updatedDiceInsideBox.splice(randomDieIndex, 1);
+      drawnDice.push(die);
     };
-    console.log(playerHand)
-    setPlayerHand(playerHand);
+    setDiceInsideBox(updatedDiceInsideBox);
+    setPlayerHand(drawnDice);
+    console.log(drawnDice)
   };
 
   const rollDice = () => {
+    let brainsRollCount = 0
+    let shotsRollCount = 0
     for (let i = 0; i < playerHand.length; i++) {
       let randomFaceIndex = Math.floor(Math.random() * 6);
       let face = playerHand[i].faces[randomFaceIndex];
-      console.log(face);
       rollResult.push(face);
-      if (face === 'brain') setBrains(brains + 1);
-      else if (face === 'shot') setShots(shots + 1);
+      if (face === 'brain') brainsRollCount += 1;
+      else if (face === 'shot') shotsRollCount += 1;
+      else diceInsideBox.push(playerHand[i]);
     };
-  console.log(rollResult)
+    setBrains(brains + brainsRollCount);
+    setShots(shots + shotsRollCount);
     setRollResult(rollResult);
     setRollCounter(rollCounter + 1);
     setPlayerHand([]);
+    console.log(rollResult)
   };
 
   const startTurn = () => {
-    setTurn(turn + 1);
+    if (turn === 0) setTurn(1);
+    setDiceInsideBox(diceBox);
     setTurnPhase('Draw');
     setPlayButtonMessage('Roll');
     drawDice();
   };
 
   const proceedTurn = () => {
-    setTurnPhase('Roll');
-    setPlayButtonMessage('Draw');
-    rollDice();
+    if (turnPhase === 'Draw') {
+      setTurnPhase('Roll');
+      setPlayButtonMessage('Draw');
+      rollDice();
+    } else {
+      setTurnPhase('Draw');
+      setPlayButtonMessage('Roll');
+      setPlayerHand([]);
+      setRollResult([]);
+      drawDice();
+    }
   };
 
   const endTurn = () => {
-    setTurnPhase('Draw');
-    setPlayerHand([]);
+    if (shots < 3) {
+      setScore(score + brains);
+      setDiceInsideBox(diceBox);
+    } else {
+      setDiceInsideBox(diceBox);
+    }
     setRollResult([]);
     setRollCounter(0);
-    startTurn();
-    setScore(score + rollResult.length)
+    setBrains(0);
+    setShots(0);
+    setPlayerHand([]);
+    setTurnPhase('');
+    setTurn(turn + 1);
   };
 
   const handlePlayButtonClick = () => {
-    if (turnPhase === 'Draw') proceedTurn();
-    else if (turnPhase === 'Roll') rollDice();
+    if (turnPhase !== '') proceedTurn();
     else startTurn();
+  };
+
+  const handleSkipButtonClick = () => {
+    setDiceInsideBox([...diceBox]);
+    endTurn();
   };
 
 
@@ -97,14 +124,20 @@ const GameStage = () => {
     const skipButton = skipButtonRef.current;
   });
 
+  useEffect(() => {
+    if (shots >= 3) endTurn();
+  }, [shots]);
 
 
   return (
     <div className={style['container']}>
-      {playerHand.length > 0 && turnPhase === 'Draw' && playerHand.map((die) => <span key={uuidv4()}>{die.color}</span>)}
-      {rollResult.length > 0 && turnPhase === 'Roll' && rollResult.map((face) => <span key={uuidv4()}>{face}</span>)}
-      <button ref={playButtonRef} onClick={handlePlayButtonClick}>{playButtonMessage}</button>
-      <button ref={skipButtonRef} onClick={endTurn} style={{ visibility: turn === 0 ? 'hidden' : 'visible' }}>End Turn</button>
+      <div className={style['screen']} style={{ visibility: turn === 0 ? 'hidden' : 'visible' }}>
+        {playerHand.length > 0 && score < 13 && turnPhase === 'Draw' && playerHand.map((die) => <span key={uuidv4()}>{die.color}</span>)}
+        {rollResult.length > 0 && score < 13 &&turnPhase === 'Roll' && rollResult.map((face) => <span key={uuidv4()}>{face}</span>)}
+        {score >= 13 && <h2>Victory!</h2>}
+      </div>
+      {score <= 13 &&<button ref={playButtonRef} onClick={handlePlayButtonClick}>{playButtonMessage}</button>}
+      {score <= 13 &&<button ref={skipButtonRef} onClick={handleSkipButtonClick} style={{ visibility: turnPhase === '' ? 'hidden' : 'visible' }}>End Turn</button>}
     </div>
   );
 };
