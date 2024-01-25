2ecefc347a30af10dc4b1a72f1049bb6c4f1fdac
nuno-oliveira <nuno.oliveira.1991@gmail.com>
2024-01-25 17:16:33 +0000

    unneccessary lines erased on Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,8 @@
 import style from './home-styles.module.scss';
-import { diceBox } from './../../constants';
-import ScoreBoard from './../../components/ScoreBoard/ScoreBoard';
-import GameStage from './../../components/GameStage/GameStage';
+import ScoreBoard from '../../components/ScoreBoard/ScoreBoard';
+import GameStage from '../../components/GameStage/GameStage';
 
 const Home = () => {
-
   return (
     <div className={style['container']}>
       <ScoreBoard />
